cc5cc028db3555507b2758a9cafd541233a305b7
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-11-07 15:15:24 -0600

    fix - Player movement align to grid

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust this to set the movement speed.
+    public float moveTime = 1f; // Time to move 1 unit.
+    private bool isMoving = false;
 
     private Rigidbody2D rb; // Use Rigidbody2D for 2D movement.
 
@@ -13,14 +15,58 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // Input from the player.
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Check if the player is already moving.
+        if (!isMoving)
+        {
+            // Input from the player.
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        // Calculate the movement direction.
-        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized;
+            // Calculate the movement direction.
+            Vector2 movement;
 
-        // Move the player using Rigidbody2D.
-        rb.velocity = movement * moveSpeed;
+            // Check if the horizontal or vertical input is significant (not close to zero).
+            if (Mathf.Abs(horizontalInput) > Mathf.Abs(verticalInput))
+            {
+                movement = new Vector2(horizontalInput, 0f).normalized; // Horizontal movement.
+            }
+            else
+            {
+                movement = new Vector2(0f, verticalInput).normalized; // Vertical movement.
+            }
+
+            if (movement != Vector2.zero)
+            {
+                // Lock input to prevent further movement during the current move.
+                isMoving = true;
+                StartCoroutine(MovePlayer(movement));
+            }
+        }
+    }
+
+    IEnumerator MovePlayer(Vector2 direction)
+    {
+        Vector2 startPos = rb.position;
+        Vector2 endPos = startPos + direction;
+
+        float startTime = Time.time;
+        float journeyLength = Vector2.Distance(startPos, endPos);
+        float speed = journeyLength / moveTime;
+
+        while (Time.time < startTime + moveTime)
+        {
+            float distanceCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distanceCovered / journeyLength;
+
+            rb.position = Vector2.Lerp(startPos, endPos, fractionOfJourney);
+
+            yield return null;
+        }
+
+        // Ensure the player ends up at the exact destination.
+        rb.position = endPos;
+
+        // Unlock input after completing the move.
+        isMoving = false;
     }
 }
